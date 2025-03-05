9cac2824e5b2550096c66121a06a9dbba75f6816
p9648213 <p9648213750912@gmail.com>
2025-03-05 07:59:41 +0700

    draw ship

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,5 @@
-use bevy::{
-    DefaultPlugins,
-    app::{App, Startup, Update},
-    ecs::{
-        component::Component,
-        query::With,
-        schedule::IntoSystemConfigs,
-        system::{Commands, Query},
-    },
-};
+use asteroids_bevy::run;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
-        .run();
-}
-
-fn hello_world() {
-    println!("hello world");
-}
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Custard_Beard".to_owned())));
-    commands.spawn((Person, Name("Stacking".to_owned())));
-    commands.spawn((Person, Name("epicblaargh".to_owned())));
-}
-
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for Name(name) in &query {
-        println!("hello {}!", name);
-    }
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0.contains('_') {
-            name.0 = name.0.replace('_', " ");
-            break;
-        }
-    }
+    run();
 }
